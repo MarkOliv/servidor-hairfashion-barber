46f1027b61552739ec5d015bf08842e0d6d08371
Marcos Oliveira <100429171+MarkOliv@users.noreply.github.com>
2023-01-05 10:42:45 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 import { createClient } from "@supabase/supabase-js";
 import * as OneSignal from "@onesignal/node-onesignal";
 
+var express = require('express');
+var app     = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+//For avoidong Heroku $PORT error
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
+
 // Onesignal notifications configuration...
 const ONESIGNAL_APP_ID = "be52199d-a047-430a-b3fd-e3ba2cb55d6d";
 const ONESIGNAL_API_REST_KEY = {
